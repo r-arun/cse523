150988aa6620ebbcb1e801f7c231ef4f7d35b0f4
arun <rathakrishnanarun@gmail.com>
2013-09-19 15:40:39 -0400

    convex property holds

--- points/convex_hull.cpp
+++ points/convex_hull.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
 #include <stack>
 #include <cstdlib>
 #include <cassert>
 #include <ctime>
+#include <cmath>
 #include <algorithm>
+#define TEST_BETWEEN 0
+#define TEST_FAILED_CASE 0
+#define TEST_RANDOM_POINTS 1
+#define TEST_CONVEX_HULL 1
+#define DEBUG 1
 using namespace std;
 class point{
 	public:
 		int x;
 		int y;
 	point(){
-		x = rand()%20;
-		y = rand()%20;
+		x = rand()%2000;
+		y = rand()%2000;
 	}
 	point(int x1, int y1){
 		x = x1;
 		y = y1;
 	}
-
+	point * upper_point(){
+		return new point(x, y + 2);
+	}
 	int crossproduct(point * p1, point * p2){
 		return (p1->x - x) * (p2->y - y) - (p2->x - x) * (p1->y - y);
 	}
@@ -31,6 +41,16 @@ class point{
 		if(y == other.y && x < other.x)
 			return true;
 		return false;
+	}
+	bool between(point * p1, point * p2){
+		return ((this->crossproduct(p1, p2) == 0) &&
+				((p1->x <= x && x <= p2->x && p1->y <= y && y <= p2->y) ||
+				(p2->x <= x && x <= p1->x && p2->y <= y && y <= p1->y) ||
+				(p1->x <= x && x <= p2->x && p2->y <= y && y <= p1->y) ||
+				(p2->x <= x && x <= p1->x && p1->y <= y && y <= p2->y)));
+	}
+	double distance(point * other){
+		return sqrt(pow(x-other->x,2) + pow(y-other->y,2));
 	}
 };
 void print(vector <point *> v){
@@ -58,12 +78,24 @@ void rearrange_for_lowest(vector <point *> & v){
 	v[pos] =  v[0];
 	v[0] = low;
 }
+
 int partition(vector <point *> & v, int ind, int start, int limit){
+	//Sort by angle :counter-clockwise first
+	//If angles are equal, sort by distance from v[ind]
 	point * pivot = v[start];
 	int j = start;
 	for(int i = start+1; i < limit; ++i){
 		int turn = v[ind]->crossproduct(v[i], pivot);
-		if(turn > 0){
+		if(turn >= 0){
+			if(turn == 0){
+				//put the closer point first
+				//our removal method for duplicates with same angle
+				//retains only the farthest point. So if angles are the same
+				//we then sort by closest point
+				if(v[ind]->distance(v[i]) > v[ind]->distance(pivot)){
+					continue;
+				}
+			}
 			++j;
 			point * temp = v[i];
 			v[i] = v[j];
@@ -89,7 +121,12 @@ double area_of_triangle(point * p1, point * p2, point * p3){
 	return abs((p2->x * p1->y - p1->x * p2->y + p3->x * p2 -> y -
 			p2->x * p3->y + p1->x * p3->y - p3 ->x * p1->y)/2);
 }
+
 double area_of_polygon(vector<point *> v){
+	if(v.size() < 3){
+		//happens when you split a quadrilateral. Does not contain an area.
+		return 0.0;
+	}
 	if(v.size() == 3){
 		return area_of_triangle(v[0], v[1], v[2]);
 	}
@@ -103,68 +140,26 @@ double area_of_polygon(vector<point *> v){
 	}
 	return area += area_of_polygon(poly);
 }
-int main(){
-	srand(time(NULL));
-	/*
-	point *p0 = new point(6, 0);
-	point *p1 = new point(12, 1);
-	point *p2 = new point(11, 2);
-	point *p3 = new point(13, 4);
-	point *p4 = new point(10, 4);
-	point *p5 = new point(9, 5);
-	point *p6 = new point(8, 5);
-	point *p7 = new point(7, 4);
-	point *p8 = new point(6, 3);
-	point *p9 = new point(5, 4);
-	point *p10 = new point(7, 12);
-	point *p11 = new point(4, 4);
-	point *p12 = new point(3, 2);
-	point *p13 = new point(3, 1);
-	cout << p0->crossproduct(p1, p2)<<endl;
-	cout << p0->crossproduct(p2, p1)<<endl;
-	cout << p0->crossproduct(p3, p4)<<endl;
-	cout << p0->crossproduct(p4, p3)<<endl;
-	vector<point *> v;
-	stack<point *> st;
-	v.push_back(p0);
-	v.push_back(p6);
-	v.push_back(p4);
-	v.push_back(p9);
-	v.push_back(p1);
-	v.push_back(p5);
-	v.push_back(p12);
-	v.push_back(p11);
-	v.push_back(p8);
-	v.push_back(p3);
-	v.push_back(p7);
-	v.push_back(p10);
-	v.push_back(p2);
-	v.push_back(p13);
-	*/
-	int sz;
-	cin>>sz;
-	vector<point *> v;
-	stack<point *> st;
-	while(sz--){
-		v.push_back(new point());	
-	}
-	cout<<"First "<<endl;
-	v[0]->display();
-	rearrange_for_lowest(v);
-	cout<<"After "<<endl;
-	v[0]->display();
-	vector <point *> :: iterator it;
-	cout<<"Before Sorting"<<endl;
-	for(it = v.begin(); it < v.end(); ++it){
-		(*it)->display();
-	}
 
+void remove_duplicate_points(vector <point *> & v){
+	if(v.size() < 3) return;
+	for(int i = 1; i < v.size()-1; ++i){
+		if(v[i]->between(v[0],v[i+1])){
+			v.erase(v.begin() + i);	
+			--i;
+		}
+	}
+}
+
+void preprocess_vector(vector <point *> & v){
+	rearrange_for_lowest(v);	
 	sort(v, 0, 1, v.size());
+	remove_duplicate_points(v);	
+}
 
-	cout<<"After Sorting"<<endl;
-	for(it = v.begin(); it < v.end(); ++it){
-		(*it)->display();
-	}
+
+vector<point *> find_convex_hull(vector<point *> v){
+	stack<point *> st;
 	st.push(v[0]);
 	st.push(v[1]);
 	st.push(v[2]);
@@ -173,6 +168,7 @@ int main(){
 		st.pop();
 		point * lst1;
 		do{
+			assert(!st.empty());
 			lst1 = st.top();
 			st.pop();
 			if(lst1->crossproduct(lst, v[i]) <= 0){
@@ -187,18 +183,127 @@ int main(){
 		}while(true);
 	}
 	vector <point *> poly;
-	cout << "The convex hull "<<endl;
 	while(!st.empty()){
-		(st.top())->display();
 		poly.push_back(st.top());
 		st.pop();
 	}
 	reverse(poly.begin(), poly.end());
-	cout<<"Printing..."<<endl;
+	return poly;
+}
+void test_between(){
+	point *p0 = new point(3, 3);
+	point *p1 = new point(4, 4);
+	point *p2 = new point(5, 5);
+	point *p3 = new point(10, 10);
+	point *p4 = new point(9, 11);
+	point *p5 = new point(8, 12);
+	point *p6 = new point(7, 13);
+	point * p7 = p2->upper_point();
+	//positive slope
+	assert(p2->crossproduct(p1, p0) == 0);
+	assert(p1->between(p0,p2));
+	assert(!p3->between(p0,p2));
+	assert(p2->between(p0,p3));
+	//non-collinear point
+	assert(!p7->between(p0,p3));
+	//negative slope
+	assert(p4->between(p3,p5));
+	assert(!p6->between(p3,p5));
+	assert(p5->between(p3,p6));
+
+	point * p10 = new point(2,12);
+	point * p11 = new point(1,11);
+	point * p12 = new point(12,19);
+	cout<<p10->crossproduct(p11, p12)<<endl;
+	assert(p10->crossproduct(p11, p12) > 0);
+
+	point * p20 = new point(1,2);
+	point * p21 = new point(0,9);
+	point * p22 = new point(3,0);
+	cout<<p20->crossproduct(p21, p22)<<endl;
+	assert(p20->crossproduct(p21, p22) > 0);
+}
+
+void test_failed_case(){
+	point *p0 = new point(2, 19);
+	point *p1 = new point(17, 15);
+	point *p2 = new point(7, 5);
+	point *p3 = new point(9, 8);
+	point *p4 = new point(8, 12);
+	point *p5 = new point(0, 6);
+	point *p6 = new point(1, 9);
+	point *p7 = new point(15, 3);
+	vector <point *> v;
+	v.push_back(p0);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	v.push_back(p5);
+	v.push_back(p6);
+	v.push_back(p7);
+	rearrange_for_lowest(v);
+	sort(v, 0, 1, v.size());
+	vector <point *> poly  = find_convex_hull(v);
+	cout << "The convex hull "<<endl;
 	print(poly);
-	//cout<<area_of_triangle(p0,p1, p3)<<endl;
-	//cout<<area_of_triangle(p3,p10,p12)<<endl;
-	//cout<<area_of_triangle(p0,p3, p12)<<endl;
-	//cout<<area_of_triangle(p0,p12, p13)<<endl;
+}
+vector <point *> read_points_from_file(string name){
+	ifstream inp;
+	vector <point *> v;
+	inp.open(name.c_str());
+	int cnt;
+	inp >> cnt;
+	while(cnt--){
+		int a, b;
+		inp >> a >> b;
+		v.push_back(new point(a, b));
+	}
+	preprocess_vector(v);
+	return  find_convex_hull(v);
+}
+
+vector<point *> random_points_test(int size){
+	vector<point *> v;
+	while(size--){
+		v.push_back(new point());	
+	}
+	preprocess_vector(v);
+#if DEBUG
+	cout<<"Initial Points "<<endl;
+	print(v);	
+#endif
+	return  find_convex_hull(v);
+}
+void test_convex_hull(vector <point *> v){
+	assert(v[0]->crossproduct(v[v.size()-1],v[1]) < 0);
+	for(int i = 1; i < v.size(); ++i){
+		 assert(v[i]->crossproduct(v[i-1],v[(i+1)%v.size()]) < 0);
+	}
+}
+int main(){
+	srand(time(NULL));
+	vector<point *> poly;
+#if TEST_BETWEEN
+	test_between();
+#endif
+#if TEST_FAILED_CASE
+	test_failed_case();
+#endif
+#if TEST_RANDOM_POINTS
+	int _sz;
+	cin>>_sz;
+	poly = random_points_test(_sz);
+#else
+	string str;
+	cin>>str;
+	poly = read_points_from_file(str);
+#endif
+	cout<<"Convex Hull"<<endl;
+	print(poly);
+#if TEST_CONVEX_HULL
+	test_convex_hull(poly);
+#endif
 	cout<<area_of_polygon(poly)<<endl;
+	return 0;
 }
